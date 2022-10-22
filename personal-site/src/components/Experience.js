2d8599a9de246c3edc621b1ee4070bd66295cac8
ant52ho <87661562+ant52ho@users.noreply.github.com>
2022-10-22 11:45:33 -0400

    experiences styling

--- personal-site/src/components/Experience.js
+++ personal-site/src/components/Experience.js
@@ -1,6 +1,8 @@
 import Col from "react-bootstrap/Col";
 import Row from "react-bootstrap/Row";
+import Button from "react-bootstrap/Button";
 import Technologies from "./Technologies";
+import { FaArrowRight } from "react-icons/fa";
 import "./Experience.css";
 
 // import reactLogo from "../logo.svg";
@@ -13,19 +15,49 @@ const Experience = ({
   date,
   technologies,
   subtitle,
+  body,
+  link,
 }) => {
+  var contentBody = body;
+  if (body) {
+    contentBody = body.split(/\n */);
+  }
   return (
     <>
       <Row className="my-2 shadowBorder oddSpacing">
         <Col className="experienceLogoContainer col-auto">
           <img src={logo} className="experienceLogo py-4"></img>
         </Col>
-        <Col className="experienceText py-3">
+        <Col className="experienceContent py-3">
           <h4 className="m-0">{title}</h4>
-          <p className="m-0">{employer}</p>
-          <p className="m-0">{date}</p>
-          <Technologies technologies={technologies} />
+          <p className="m-0 companyDate d-none d-md-block">
+            {employer} {date ? "|" : null} {date}
+          </p>
+          <p className="m-0 companyDate d-block d-md-none">{employer}</p>
+          <p className="m-0 companyDate d-block d-md-none">{date}</p>
           <p className="m-0">{subtitle}</p>
+          {body ? (
+            <ul className="pt-2 m-0 experienceBody d-none d-md-block">
+              {contentBody.map((item, i) => (
+                <li key={i}>{item}</li>
+              ))}
+            </ul>
+          ) : null}
+          <div className="py-2 d-none d-md-block">
+            <Technologies technologies={technologies} />
+          </div>
+          <div className="d-flex justify-content-end pb-1 pt-3">
+            <a href={link} target="_blank" className="readMore py-1">
+              <Button
+                variant="outline-secondary"
+                className="readMore"
+                size="lg"
+              >
+                Read More <FaArrowRight />
+              </Button>
+              {""}
+            </a>
+          </div>
         </Col>
       </Row>
     </>
--- personal-site/src/components/Experiences.js
+++ personal-site/src/components/Experiences.js
@@ -10,21 +10,38 @@ const Experiences = () => {
   return (
     <>
       <Container className="py-2 experiences" id="experiences">
-        <h1>Experience</h1>
-        {/* <div className="shadowBorder my-3"> */}
+        <h1 className="pb-3">Experience</h1>
+        <Experience
+          logo={uwLogo}
+          title="Tunnel Systems Research Assistant"
+          employer="University of Waterloo"
+          date="May 2022 ~ Present"
+          technologies={[
+            "Python",
+            "Bash",
+            "JavaScript",
+            "AWS",
+            "Redis",
+            "SQLite",
+            "React",
+          ]}
+          body={`Engineered an environment monitoring IoT system for industrial use using Raspberry Pis and AWS EC2, capable of scaling into multiple tunnel networks.
+          Supported a dynamic number of nodes and consistent node identification using a customized static DHCP IP assignment protocol, designed for "plug and play" immediate installation.
+          Designed a Redis primary database that aggregates datapoints by time-based downsampling, increasing data storage efficiency by 1800%.
+          Implemented an interactive dashboard with React + Express containing data visualization of real-time sensor data and user alerts during emergencies.`}
+          link="https://google.com"
+        />
         <Experience
           className=""
           logo={uwLogo}
-          title="Research Assistant"
+          title="Stereo / Computer Vision Research Assistant"
           employer="University of Waterloo"
-          date="May 2022 ~ Present"
-          technologies={["Python", "Bash", "SQLite", "Redis", "React", "AWS"]}
+          date="July 2019 ~ August 2019"
+          technologies={["Python"]}
+          body={`Researched, implemented stereo vision models after analyzing multiple research papers using OpenCV, resulting in a passable "ground truth" output.
+          Augmented training data using OpenCV and improved the efficacy of a colleague's CNN stereo model.`}
         />
-        {/* <hr className="m-1" /> */}
-        <Experience className="" logo={uwLogo} />
-        {/* <hr className="m-1" /> */}
-        <Experience className="" logo={uwLogo} />
-        {/* </div> */}
+        {/* <Experience className="" logo={uwLogo} /> */}
       </Container>
     </>
   );
