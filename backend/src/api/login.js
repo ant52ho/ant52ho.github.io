5f87239eaa2382934d687d844eceec806b355e0f
antho <anthony52ho@gmail.com>
2024-01-13 13:20:47 -0500

    returns userrole in jwt token

--- backend/src/api/login.js
+++ backend/src/api/login.js
@@ -5,36 +5,40 @@ const jwt = require("jsonwebtoken");
 const router = express.Router();
 
 router.post("/login", async (req, res) => {
-    const { email, password } = req.body;
+  const { email, password } = req.body;
 
-    const userWithEmail = await User.findOne({ where: { email } }).catch(
-        (err) => {
-            console.log("Error:", err);
-        }
-    );
-
-    if (!userWithEmail) {
-        console.log("failed to login");
-
-        return res
-            .status(400)
-            .json({ message: "Email or password does not match" });
+  const userWithEmail = await User.findOne({ where: { email } }).catch(
+    (err) => {
+      console.log("Error:", err);
     }
+  );
 
-    if (userWithEmail.password !== password) {
-        console.log("failed to login");
-        return res
-            .status(400)
-            .json({ message: "Email or password does not match" });
-    }
+  if (!userWithEmail) {
+    console.log("failed to login");
 
-    const jwtToken = jwt.sign(
-        { id: userWithEmail.id, email: userWithEmail.email },
-        process.env.JWT_SECRET
-    );
+    return res
+      .status(400)
+      .json({ message: "Email or password does not match" });
+  }
 
-    console.log("Login success");
-    res.json({ message: "Welcome back!", token: jwtToken });
+  if (userWithEmail.password !== password) {
+    console.log("failed to login");
+    return res
+      .status(400)
+      .json({ message: "Email or password does not match" });
+  }
+
+  const jwtToken = jwt.sign(
+    {
+      id: userWithEmail.id,
+      email: userWithEmail.email,
+      userRole: userWithEmail.userRole,
+    },
+    process.env.JWT_SECRET
+  );
+
+  console.log("Login success");
+  res.json({ message: "Welcome back!", token: jwtToken });
 });
 
 module.exports = router;
