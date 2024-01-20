40a73f9ca29266f35901c0f234882c1ebaaeb2a0
antho <anthony52ho@gmail.com>
2024-01-20 00:58:01 -0500

    created jwt cookie decoding middleware function!

--- backend/src/middlewares.js
+++ backend/src/middlewares.js
@@ -1,22 +1,45 @@
+const { jwtDecode } = require("jwt-decode");
+
 function notFound(req, res, next) {
-    res.status(404);
-    const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
-    next(error);
+  res.status(404);
+  const error = new Error(`🔍 - Not Found - ${req.originalUrl}`);
+  next(error);
 }
 
 /* eslint-disable no-unused-vars */
 function errorHandler(err, req, res, next) {
-    /* eslint-enable no-unused-vars */
-    const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
-    res.status(statusCode);
-    res.json({
-        message: err.message,
-        stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
-    });
-    console.log(err);
+  /* eslint-enable no-unused-vars */
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+  console.log(err);
+}
+
+function addJWTtoCookie(req, res, next) {
+  try {
+    if (req?.cookies === undefined) {
+      next();
+    }
+
+    console.log(req.cookies);
+
+    const decoded = jwtDecode(req.cookies["_auth"]);
+    req.cookies = {
+      ...req.cookies,
+      ...decoded,
+    };
+  } catch (err) {
+    // console.log(err);
+    console.log("No cookie available to parse");
+  }
+  next();
 }
 
 module.exports = {
-    notFound,
-    errorHandler,
+  notFound,
+  errorHandler,
+  addJWTtoCookie,
 };
