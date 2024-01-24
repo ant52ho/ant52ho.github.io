ac677af4839318b29bfc939d9dc2ecc7c58ff3eb
antho <anthony52ho@gmail.com>
2024-01-24 00:49:21 -0500

    made username unique -> should update in mysql

--- backend/src/models/user.js
+++ backend/src/models/user.js
@@ -4,9 +4,15 @@ const { DataTypes } = require("sequelize");
 const sequelize = require("../database");
 
 const User = sequelize.define("User", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    unique: true,
+  },
   fullName: {
     type: DataTypes.STRING,
     allowNull: false,
+    unique: true,
   },
   email: {
     type: DataTypes.STRING,
